ab7dc17abd1b5ffdbe8d60259bbd1bbc6e2e5669
DConforti <dominic.conforti@smail.rasmussen.edu>
2025-11-23 13:59:33 -0600

    Add files via upload

---
+++ assignment2.js
@@ -0,0 +1,8 @@
+let fName = prompt("Please enter your first name.");
+alert("Welcome, " + fName + "!");
+
+const piValue = 3.1415926;
+let myFavNum = prompt("Please enter your favorite number.");
+let myArea = piValue * myFavNum * myFavNum;
+
+alert("Hello " + fName + ", you entered " + myFavNum + " as your favorite number. If that was the radius of a circle, the circle's area would be " + myArea + ".");
